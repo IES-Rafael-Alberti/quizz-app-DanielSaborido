f9fa2ba159115ddb3c0086176d202b45a1f15e4d
DanielSaborido <dsaborio@iesrafaelalberti.es>
2024-01-22 19:11:24 +0100

    datos de la base de datos

--- codigophp/db.sql
+++ codigophp/db.sql
@@ -21,3 +21,18 @@ CREATE TABLE IF NOT EXISTS questions
     correct_option CHAR(1),
     FOREIGN KEY (quiz_id) REFERENCES quiz(quiz_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+INSERT INTO quiz (title, description, quiz_user) VALUES
+    ('PHP Quiz', 'Test your knowledge on PHP concepts.', 'Daniel Saborido');
+
+INSERT INTO questions (quiz_id, question_text, option_a, option_b, option_c, correct_option) VALUES
+    (1, 'What does PHP stand for?', 'Personal Home Page', 'PHP: Hypertext Preprocessor', 'PHP Hyper Markup Language', 'b'),
+    (1, 'What is the result of the following code? $x = 5; echo ++$x + $x++;', '11', '12', '13', 'c'),
+    (1, 'How do you declare a static method in a PHP class?', 'static function methodName() {}', 'function static methodName() {}', 'function methodName() static {}', 'a'),
+    (1, 'What is the purpose of the PHP function `htmlspecialchars()`?', 'Converts special characters to HTML entities', 'Parses HTML code', 'Encodes URLs', 'a'),
+    (1, 'How can you initiate a session in PHP?', 'start_session()', 'session_start()', 'init_session()', 'b'),
+    (1, 'What is the purpose of the `implode()` function in PHP?', 'Splits a string into an array', 'Joins array elements into a string', 'Finds the length of a string', 'b'),
+    (1, 'In PHP, what is the difference between `==` and `===`?', '`==` checks for value equality, `===` checks for value and type equality', '`==` checks for value and type equality, `===` checks for value equality', 'There is no difference', 'a'),
+    (1, 'How can you prevent SQL injection in PHP?', 'Use prepared statements', 'Sanitize user input', 'Both a and b', 'c'),
+    (1, 'What is the purpose of the `unset()` function in PHP?', 'Deletes a file', 'Unsets a variable', 'Removes an array element', 'b'),
+    (1, 'How can you include an external PHP file?', 'include("file.php")', 'require("file.php")', 'Both a and b', 'c');
